9ae6075466e70b337a5394572861b27a3bef15fa
zhaodh <zhaodh@hdu.edu.cn>
2023-04-20 20:17:35 +0800

    chat page complete

--- src/components/chats/index.vue
+++ src/components/chats/index.vue
@@ -1,13 +1,41 @@
 <template>
     <div style="height: 100vh;">
-        
+        <PrettyChatWindow
+            :projectId="projectId"
+            :username="username"
+            :secret="secret" />
     </div>
 </template>
 <script>
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
+import { applyReactInVue } from 'veaury';
+
 export default {
-    name: 'ChatPage'
+    name: 'ChatPage',
+    data() {
+        return {
+            projectId: import.meta.env.VITE_CHATENGINE_PROJECT_ID,
+        };
+    },
+    components: {
+        PrettyChatWindow: applyReactInVue(PrettyChatWindow)
+    },
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        secret: {
+            type: String,
+            required: true
+        }
+    }
 }
 </script>
-<style lang="">
-    
+<style lang="" scoped>
+  .ce-new-chat-button {
+    width: 32px;
+    position: relative;
+    bottom: 22px;
+  }  
 </style>
